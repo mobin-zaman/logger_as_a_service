5f0c9d4bfa6d07a164338fe79fb7abcc7b4396da
mobin_zaman <mobinzamandev@gmail.com>
2020-07-15 11:37:07 +0600

    authentication  working

---
+++ app/Http/Controllers/JWTAuthController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Validator;
+
+class JWTAuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|between:2,100',
+            'email' => 'required|email|unique:users|max:50',
+            'password' => 'required|string:min:6'
+        ]);
+
+
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create(array_merge(
+            $validator->validated(),
+            ['password' => bcrypt($request->password)]
+        ));
+
+        return response()->json([
+            'messages' => 'Successfully registered',
+            'user' => $user
+        ],201);
+    }
+
+    public function login(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+
+        if(! $token = auth('api')->attempt($validator->validated())) {
+            return response()->json(['error' =>'Unauthorized'], 401);
+        }
+
+        return $this->createNewToken($token);
+    }
+
+    public function profile()
+    {
+        return response()->json(auth()->user());
+    }
+
+
+    public function logout()
+    {
+        auth()->logout();
+        return response()->json(['message' =>'Successfully logged out']);
+    }
+
+    public function refresh(){
+        return $this->createNewToken(auth('api')->refresh());
+    }
+
+    protected function createNewToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+], function ($router) {
+
+    Route::post('login','JWTAuthController@login');
+    Route::post('register', 'JWTAuthController@register');
+    Route::post('logout', 'JWTAuthController@logout');
+    Route::post('refresh', 'JWTAuthController@refresh');
+    Route::get('profile', 'JWTAuthController@profile');
+
 });
