1b28f591dba8e625946ec0489df968dd41bb3adb
mobin_zaman <mobinzamandev@gmail.com>
2020-07-16 20:30:02 +0600

    logs table changed

--- app/Log.php
+++ app/Log.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Log extends Model
 {
+
+    protected $fillable = ['application_id','type', 'description'];
     public function application(){
         $this->belongsTo(Application::class);
     }
---
+++ database/migrations/2020_07_16_141651_alter_logs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('logs', function (Blueprint $table) {
+            //NOTE: to change the datatype
+            $table->text('description')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('logs', function (Blueprint $table) {
+            //
+        });
+    }
+}
